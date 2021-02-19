4eba4ce0dc728f2b984dfafcf0f8f05fd00e26d9
ElricM <elric.milton@yahoo.com.au>
2021-02-20 10:04:29 +1100

    Added folders

---
+++ New Unity Project/Assets/Scripts/ChildOnCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildOnCollision : MonoBehaviour
+{
+    SphereCollider col;
+    [SerializeField] float sizeIncrease = 0.2f;
+
+    void Start()
+    {
+        col = gameObject.GetComponent<SphereCollider>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Collectable"))
+        {
+            ContactPoint contact = collision.contacts[0];
+            Vector3 pos = contact.point;
+            GameObject ob = collision.gameObject;
+            ob.transform.parent = gameObject.transform;
+            ob.transform.position = pos;
+            ob.GetComponent<Collider>().enabled = false;
+            col.radius += sizeIncrease;
+            //collision.gameObject.transform.localPosition
+        }
+    }
+}
