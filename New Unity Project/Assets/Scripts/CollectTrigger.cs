e59849b52deb9f74042ee7e5b903edcd294cd374
ElricM <elric.milton@yahoo.com.au>
2021-02-20 23:05:47 +1100

    Created lots of animated people with anim transitions

---
+++ New Unity Project/Assets/Scripts/CollectTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectTrigger : MonoBehaviour
+{
+    public AnimatorOverrideController animatorOverride;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            anim.runtimeAnimatorController = animatorOverride;
+
+        }
+    }
+}
