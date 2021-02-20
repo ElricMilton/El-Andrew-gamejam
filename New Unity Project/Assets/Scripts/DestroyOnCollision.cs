1eedd30e4f98a5f7e01c1a639c0d247a22d3f945
ElricM <elric.milton@yahoo.com.au>
2021-02-20 20:21:24 +1100

    Some stuff

--- New Unity Project/Assets/Scripts/DestroyOnCollision.cs
+++ New Unity Project/Assets/Scripts/DestroyOnCollision.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour
 {
+    GameObject player;
+    Transform resetPos;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Start()
+    {
+        resetPos = player.transform;
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        Object.Destroy(collision.gameObject);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.position = resetPos.position;
+        }
+        else { Object.Destroy(collision.gameObject); }
     }
 }
