cd740500e957d89fdad9dc4f2cc8b9f6fbea8a93
ElricM <elric.milton@yahoo.com.au>
2021-02-20 23:29:45 +1100

    updated collectible prefabs

--- New Unity Project/Assets/Scripts/CollectTrigger.cs
+++ New Unity Project/Assets/Scripts/CollectTrigger.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class CollectTrigger : MonoBehaviour
 {
     public AnimatorOverrideController animatorOverride;
     Animator anim;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        rb = gameObject.GetComponent<Rigidbody>();
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
             anim.runtimeAnimatorController = animatorOverride;
-
+            rb.isKinematic = true;
         }
     }
 }
