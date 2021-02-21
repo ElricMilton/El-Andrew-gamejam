199931526c0febc3c9f8fa6983456ee715972ab3
ElricM <elric.milton@yahoo.com.au>
2021-02-21 21:22:23 +1100

    stuff

--- New Unity Project/Assets/Scripts/ChildOnCollision.cs
+++ New Unity Project/Assets/Scripts/ChildOnCollision.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class ChildOnCollision : MonoBehaviour
