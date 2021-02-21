37274399234eacac43d9b0d908ac3f2ec4dc8c15
ElricM <elric.milton@yahoo.com.au>
2021-02-21 19:59:28 +1100

    Added collection particle effect

--- New Unity Project/Assets/Scripts/CollectTrigger.cs
+++ New Unity Project/Assets/Scripts/CollectTrigger.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CollectTrigger : MonoBehaviour
 {
     public AnimatorOverrideController animatorOverride;
+    GameObject particleOb;
+    ParticleSystem particle;
     Animator anim;
     Rigidbody rb;
     Collider col;
+    Transform partPos;
     // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
         rb = gameObject.GetComponent<Rigidbody>();
         col = gameObject.GetComponent<Collider>();
+        particleOb = GameObject.FindGameObjectWithTag("Particle");
+        partPos = particleOb.transform;
+        particle = particleOb.GetComponent<ParticleSystem>();
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -28,6 +35,8 @@ public class CollectTrigger : MonoBehaviour
             gameObject.transform.parent = ob.transform;
             gameObject.transform.position = pos;
             col.enabled = false;
+            partPos.position = pos;
+            particle.Play();
         }
     }
 }
